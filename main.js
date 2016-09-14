584f389f01ae75b43a26a2fb8c200e6296c58d9e
Gavin Dmello <dmellogavin5000@gmail.com>
2016-09-15 02:54:50 +0530

    Failover for workers

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ var cluster = require('cluster');
 var numCPUs = require('os').cpus().length;
 var config = require('./config.json')
 
-
 if (config.multiCore) {
     if (cluster.isMaster) {
         for (var i = 0; i < numCPUs; i++) {
             // Create a worker
             cluster.fork();
         }
+        // restart if process dies
+        cluster.on('exit', function(worker, code, signal) {
+            cluster.fork();
+        });
     } else {
         serverInit()
     }
@@ -19,16 +22,17 @@ if (config.multiCore) {
 
 
 function serverInit() {
-
     var http
     var router = require('./router')
     var Health = require('./health').health
     var health = new Health()
     var PORT = config.port;
     var server
+
     health.ping()
 
     function handleAnyRequest(request, response) {
+        console.log(process.pid)
         router.hitServers(request, function(lbResponse) {
             // console.log(request.connection.remoteAddress)
             if (lbResponse) {
