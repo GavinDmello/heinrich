8ff69a95d89cc74609074cb7cc05584398747f4b
Temp <dmellogavin5000@gmail.com>
2016-10-31 16:07:09 +0530

    added utility to log to file

--- utilities/logger.js
+++ utilities/logger.js
@@ -1,13 +1,34 @@
-function logger() {}
+var fs = require('fs')
+
+function logger() {
+    this.directoryCheck = false
+}
 
 module.exports = logger
 
 logger.prototype.log = function() {
     if (process.env.NODE_ENV !== 'test') {
         console.log(new Date().toLocaleString(), arguments)
+        // this.logToFile('info', arguments)
     }
 }
 
 logger.prototype.error = function() {
     console.log(new Date().toLocaleString(), arguments)
+    // this.logToFile('error', arguments)
 }
+
+logger.prototype.logToFile = function(type, arguments) {
+    var logPath = '/var/log/heinrich'
+    var dataToBeLogged = new Date().toLocaleString() + ' ' + JSON.stringify(arguments) + '\n'
+    var filePath = logPath + '/' + type + '.log'
+    if (!this.directoryCheck) {
+        if (!fs.existsSync(logPath)) {
+            fs.mkdirSync(logPath);
+            this.directoryCheck = true
+        }
+        fs.appendFile(filePath, dataToBeLogged, noop)
+    }
+}
+
+function noop() {}
